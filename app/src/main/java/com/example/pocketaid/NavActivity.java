19f854f0055dc03fcc8a9142286acc42929e0222
SofiaCompSci <1809565@brunel.ac.uk>
2019-12-03 17:53:45 +0000

    Changing FAB logo

--- app/src/main/java/com/example/pocketaid/NavActivity.java
+++ app/src/main/java/com/example/pocketaid/NavActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.pocketaid.ui.Log.LogFragment;
+import com.example.pocketaid.ui.Log.LogViewModel;
 import com.example.pocketaid.ui.login.LoginActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -23,6 +25,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Toast;
 
 public class NavActivity extends AppCompatActivity {
 
@@ -38,9 +41,14 @@ public class NavActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                System.out.println("I am working");
+                Toast.makeText(getApplicationContext(), "I am working", Toast.LENGTH_LONG).show();
+
                 Snackbar.make(view, "Add to today's log", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-        }
+//                openLog(view);
+
+            }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -53,6 +61,11 @@ public class NavActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    private void openLog(View view) {
+        Intent Log = new Intent(this, LogViewModel.class);
+        startActivity(Log);
     }
 
     @Override
